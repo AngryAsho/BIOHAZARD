b5fe2473ed0bee42d8638c8b55aa5cf1e4a85bfc
Asho <angryasho@gmail.com>
2017-11-22 12:24:13 +0200

    Creating

---
+++ EX01_Crooked_Digits/DigitCrooker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EX01_Crooked_Digits
+{
+    class DigitCrooker
+    {
+        static void Main()
+        {
+            string N = Console.ReadLine();
+            double sum = 0;
+            double negativeCheck = Double.Parse(N);
+
+            if (negativeCheck < 0)
+            {
+                negativeCheck *= -1;
+                N = negativeCheck.ToString();
+            }
+            else
+            {
+                N = negativeCheck.ToString();
+            }
+
+            Logic:
+            foreach (double i in N)
+            {
+                sum += (i - 48);
+            }
+
+            if (sum > 9)
+            {
+                N = sum.ToString();
+                sum = 0;
+                goto Logic;
+            }
+            else
+            {
+                N = sum.ToString();
+                Console.WriteLine(N);
+            }
+        }
+    }
+}
---
+++ EX02_Crooked_Stairs/StairsBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace EX02_Crooked_Stairs
+{
+    class StairsBuilder
+    {
+        static void Main()
+        {
+            decimal BH1 = decimal.Parse(Console.ReadLine());
+            decimal BH2 = decimal.Parse(Console.ReadLine());
+            decimal BH3 = decimal.Parse(Console.ReadLine());
+            decimal L = decimal.Parse(Console.ReadLine());
+
+            decimal bricksPerLine = 3;
+            decimal nextHight = 0;
+
+            Console.WriteLine(BH1);
+            Console.WriteLine(BH2 + " " + BH3);
+
+            for (decimal i = 0; i < L - 2; i++)
+            {
+                for (decimal j = 0; j < bricksPerLine; j++)
+                {
+                    nextHight = (BH1 + BH2 + BH3);
+                    Console.Write(nextHight);
+                    BH1 = BH2;
+                    BH2 = BH3;
+                    BH3 = nextHight;
+                    if (j < bricksPerLine - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                bricksPerLine++;         
+            }
+        }
+    }
+}
